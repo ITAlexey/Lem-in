4e9b0e1a1484387ffdda527bd32458bba69a5789
Alexey Artemyev <alexey.artemyev96@gmail.com>
2020-10-01 11:51:08 +0600

    passed tests in case of several start and plain rooms

--- tests/validation/test_rooms.c
+++ tests/validation/test_rooms.c
@@ -63,8 +63,53 @@ static void __test_one_room__(t_farm *data, char *test_name, char *path, char co
 	bzero((void *)g_path + BIAS, SIZE - BIAS);
 }
 
+static void	__test_several_rooms__(t_farm *data, char *test_name, char *path, char *keys[], t_hashmap **type)
+{
+	short start;
+	short end;
+	t_table *plain_1;
+	t_table *plain_2;
+
+	start = 0;
+	end = 0;
+	printf("Test: %-35s", test_name);
+	data->fd = open(ft_strcat(g_path, path), O_RDONLY);
+	data->links_nbr = 0;
+	data->room_type.start_size = 0;
+	data->room_type.plain_size = 0;
+	data->room_type.end_size = 0;
+	while (data->is_err < 0 && get_next_line(data->fd, &data->line))
+	{
+		if (*(data->line) == '#')
+			define_comment(data, &start, &end);
+		else
+			define_command(data, &start, &end);
+		ft_strdel(&data->line);
+	}
+	if (data->is_err < 0
+		&& (plain_1 = get_table(*type, keys[0])) != NULL
+		&& (plain_2 = get_table(*type, keys[1])) != NULL)
+	{
+		remove_elem(*type, keys[0]);
+		remove_elem(*type, keys[1]);
+		if (!get_table(*type, keys[0]) && !get_table(*type, keys[1]))
+			success();
+		else
+			fail();
+		remove_hashmap(*type);
+	}
+	else
+		fail();
+	data->is_err = -1;
+	close(data->fd);
+	bzero((void *)g_path + BIAS, SIZE - BIAS);
+}
+
 void 	__ROOMS__(t_farm *data)
 {
+	char *plains[] = {"plain1", "plain2"};
+	char *starts[] = {"start1", "start2"};
+
 	printf("\nCheck rooms:\n");
 	__test_inv_room__(data, "Invalid coordinates", 3, ft_strcat(g_path,"room/wrong_coor"));
 	__test_inv_room__(data, "Name that start with L", 3, ft_strcat(g_path,"room/invalid_name"));
@@ -72,7 +117,7 @@ void 	__ROOMS__(t_farm *data)
 	__test_one_room__(data, "Record one plain room","room/one_plain", "plain", &data->room_type.plain);
 	__test_one_room__(data, "Record one start room", "room/one_start", "start", &data->room_type.start);
 
-	/*__test_room__(data, "Record several plain room", -1, "tests/txt/room/", 3)
-	__test_room__(data, "Record several start rooms", -1, "tests/txt/room/", 3);
-	__test_room__(data, "Record several plain, start and end rooms", -1, "tests/txt/room/", 6);*/
+	__test_several_rooms__(data, "Record several plain room", "room/several_plain", plains, &data->room_type.plain);
+	__test_several_rooms__(data, "Record several start rooms", "room/several_start", starts, &data->room_type.start);
+	//__test_all_rooms__(data, "Record several plain, start and end rooms", -1, "tests/txt/room/", 6);
 }
