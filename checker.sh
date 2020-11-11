9fa207d4ef7ea8c2fe7ec95896dbe409dd5c9094
Alexey Artemyev <alexey.artemyev96@gmail.com>
2020-11-11 18:56:41 +0600

    script file has been amended

--- checker.sh
+++ checker.sh
@@ -1,50 +1,50 @@
 #!/bin/bash
 
 echo -e "\n====One ant to one path====\n"
-./generate_map --flow-one > map
+./resources/generator --flow-one > resources/map
 echo -e "Time:"
-time ./lem-in < map > result
+time ./lem-in < resources/map > resources/result
 echo -e "\nLeaks:\n"
-valgrind --log-file=logs/leaks.log --leak-check=full --track-origins=yes ./lem-in < map > /dev/null
+valgrind --log-file=logs/leaks.log --leak-check=full --track-origins=yes ./lem-in < resources/map > /dev/null
 awk -F"== " '/ERROR SUMMARY/ {print $2}' logs/leaks.log
 echo -e ""
-grep -n "#Here" map | head -1 | awk -F"of " '{print $2}'
-echo -e "lines recieved: $(wc -l < result)"
+grep -n "#Here" resources/map | head -1 | awk -F"of " '{print $2}'
+echo -e "lines recieved: $(wc -l < resources/result)"
 
 echo -e "\n====Ten ants to one path====\n"
-./generate_map --flow-ten > map
+./resources/generator --flow-ten > resources/map
 echo -e "Time:"
-time ./lem-in < map > result
+time ./lem-in < resources/map > resources/result
 echo -e "\nLeaks:\n"
-valgrind --log-file=logs/leaks.log --leak-check=full --track-origins=yes ./lem-in < map > /dev/null
+valgrind --log-file=logs/leaks.log --leak-check=full --track-origins=yes ./lem-in < resources/map > /dev/null
 awk -F"== " '/ERROR SUMMARY/ {print $2}' logs/leaks.log
 echo -e ""
-grep -n "#Here" map | head -1 | awk -F"of " '{print $2}'
-echo -e "lines recieved: $(wc -l < result)"
+grep -n "#Here" resources/map | head -1 | awk -F"of " '{print $2}'
+echo -e "lines recieved: $(wc -l < resources/result)"
 
 echo -e "\n====Thousand ants to one path====\n"
-./generate_map --flow-thousand > map
+./resources/generator --flow-thousand > resources/map
 echo -e "Time:"
-time ./lem-in < map > result
+time ./lem-in < resources/map > resources/result
 echo -e "\nLeaks:\n"
-valgrind --log-file=logs/leaks.log --leak-check=full --track-origins=yes ./lem-in < map > /dev/null
+valgrind --log-file=logs/leaks.log --leak-check=full --track-origins=yes ./lem-in < resources/map > /dev/null
 awk -F"== " '/ERROR SUMMARY/ {print $2}' logs/leaks.log
 echo -e ""
-grep -n "#Here" map | head -1 | awk -F"of " '{print $2}'
-echo -e "lines recieved: $(wc -l < result)"
+grep -n "#Here" resources/map | head -1 | awk -F"of " '{print $2}'
+echo -e "lines recieved: $(wc -l < resources/result)"
 
 echo -e "\n=======Big (~4K rooms)===========\n"
-./generate_map --big > map
+./resources/generator --big > resources/map
 echo -e "Time:"
-time ./lem-in < map > result
+time ./lem-in < resources/map > resources/result
 echo -e ""
-grep -n "#Here" map | head -1 | awk -F"of " '{print $2}'
-echo -e "lines recieved: $(wc -l < result)"
+grep -n "#Here" resources/map | head -1 | awk -F"of " '{print $2}'
+echo -e "lines recieved: $(wc -l < resources/result)"
 
-echo -e "\n=======Complex big map===========\n"
-./generate_map --big-superposition > map
+echo -e "\n=======Complex big resources/map===========\n"
+./resources/generator --big-superposition > resources/map
 echo -e "Time:"
-time ./lem-in < map > result
+time ./lem-in < resources/map > resources/result
 echo -e ""
-grep -n "#Here" map | head -1 | awk -F"of " '{print $2}'
-echo -e "lines recieved: $(wc -l < result)"
+grep -n "#Here" resources/map | head -1 | awk -F"of " '{print $2}'
+echo -e "lines recieved: $(wc -l < resources/result)"
