58225cddddee58d7a08b64ca8a2a85bb287ba921
iles982 <iles.boss@mail.ru>
2020-11-12 18:24:50 +0600

    bash add

---
+++ vis_help.sh
@@ -0,0 +1,7 @@
+#!bin/bash
+
+echo "\033[33;01m      --Use \"lem_in -v output < 'map_name' > output\" to run with visualisation\033[0m"
+echo "\033[33;01m      --Use '1', '2', '3' to change speed of ants\033[0m"
+echo "\033[33;01m      --Use '4' & '5' to change color of ants\033[0m"
+echo "\033[33;01m      --Use right & left buttons to change ants render size\033[0m"
+echo "\033[33;01m      --Use up & down buttons to change rooms render size\033[0m"
