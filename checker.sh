9cd1927920a2bfa5aa89afe8358c67b947503359
Alexey Artemyev <alexey.artemyev96@gmail.com>
2020-11-12 19:40:12 +0600

    added colorful output when use checker

--- checker.sh
+++ checker.sh
@@ -1,50 +1,60 @@
 #!/bin/bash
 
-echo -e "\n====One ant to one path====\n"
+green='\033[32;01m'
+reset='\033[0m'
+red='\033[1;31m'
+
+echo -e "\n$green====One ant to one path====$reset\n"
 ./resources/generator --flow-one > resources/map
 echo -e "Time:"
 time ./lem-in < resources/map > resources/result
+echo -e ""
+grep -n "#Here" resources/map | head -1 | awk -F"of " '{print $2}'
+echo -e "${red}lines recieved: $(wc -l < resources/result)$reset"
 echo -e "\nLeaks:\n"
 valgrind --log-file=logs/leaks.log --leak-check=full --track-origins=yes ./lem-in < resources/map > /dev/null
 awk -F"== " '/ERROR SUMMARY/ {print $2}' logs/leaks.log
-echo -e ""
-grep -n "#Here" resources/map | head -1 | awk -F"of " '{print $2}'
-echo -e "lines recieved: $(wc -l < resources/result)"
 
-echo -e "\n====Ten ants to one path====\n"
+echo -e "\n$green====Ten ants to one path====$reset\n"
 ./resources/generator --flow-ten > resources/map
 echo -e "Time:"
 time ./lem-in < resources/map > resources/result
+echo -e ""
+grep -n "#Here" resources/map | head -1 | awk -F"of " '{print $2}'
+echo -e "${red}lines recieved: $(wc -l < resources/result)$reset"
 echo -e "\nLeaks:\n"
 valgrind --log-file=logs/leaks.log --leak-check=full --track-origins=yes ./lem-in < resources/map > /dev/null
 awk -F"== " '/ERROR SUMMARY/ {print $2}' logs/leaks.log
-echo -e ""
-grep -n "#Here" resources/map | head -1 | awk -F"of " '{print $2}'
-echo -e "lines recieved: $(wc -l < resources/result)"
 
-echo -e "\n====Thousand ants to one path====\n"
+echo -e "\n$green====Thousand ants to one path====$reset\n"
 ./resources/generator --flow-thousand > resources/map
 echo -e "Time:"
 time ./lem-in < resources/map > resources/result
+echo -e ""
+grep -n "#Here" resources/map | head -1 | awk -F"of " '{print $2}'
+echo -e "${red}lines recieved: $(wc -l < resources/result)$reset"
 echo -e "\nLeaks:\n"
 valgrind --log-file=logs/leaks.log --leak-check=full --track-origins=yes ./lem-in < resources/map > /dev/null
 awk -F"== " '/ERROR SUMMARY/ {print $2}' logs/leaks.log
-echo -e ""
-grep -n "#Here" resources/map | head -1 | awk -F"of " '{print $2}'
-echo -e "lines recieved: $(wc -l < resources/result)"
 
-echo -e "\n=======Big (~4K rooms)===========\n"
+echo -e "\n$green=======Big (~4K rooms)===========$reset\n"
 ./resources/generator --big > resources/map
 echo -e "Time:"
 time ./lem-in < resources/map > resources/result
 echo -e ""
 grep -n "#Here" resources/map | head -1 | awk -F"of " '{print $2}'
-echo -e "lines recieved: $(wc -l < resources/result)"
+echo -e "${red}lines recieved: $(wc -l < resources/result)$reset"
+echo -e "\nLeaks:\n"
+valgrind --log-file=logs/leaks.log --leak-check=full --track-origins=yes ./lem-in < resources/map > /dev/null
+awk -F"== " '/ERROR SUMMARY/ {print $2}' logs/leaks.log
 
-echo -e "\n=======Complex big resources/map===========\n"
+echo -e "\n$green=======Complex big map===========\n$reset"
 ./resources/generator --big-superposition > resources/map
 echo -e "Time:"
 time ./lem-in < resources/map > resources/result
 echo -e ""
 grep -n "#Here" resources/map | head -1 | awk -F"of " '{print $2}'
-echo -e "lines recieved: $(wc -l < resources/result)"
+echo -e "${red}lines recieved: $(wc -l < resources/result)$reset"
+echo -e "\nLeaks:\n"
+valgrind --log-file=logs/leaks.log --leak-check=full --track-origins=yes ./lem-in < resources/map > /dev/null
+awk -F"== " '/ERROR SUMMARY/ {print $2}' logs/leaks.log
--- srcs/print_solution.c
+++ srcs/print_solution.c
@@ -81,7 +81,7 @@ void			print_solution(t_path *data, int ants, int reached)
 	int			idx;
 
 	id_ant = 1;
-	output = init_hashmap(MAX_SIZE / 2, NULL);
+	output = init_hashmap(MAX_SIZE / 3, NULL);
 	while (reached < ants)
 	{
 		idx = 0;
